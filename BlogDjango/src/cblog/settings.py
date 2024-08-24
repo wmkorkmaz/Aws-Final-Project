a400cc0f338a2dc48b26f521d5cbdecc91d88bfa
wmkorkmaz <124524338+wmkorkmaz@users.noreply.github.com>
2024-08-24 15:22:36 +0200

    Update settings.py

--- BlogDjango/src/cblog/settings.py
+++ BlogDjango/src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'xxxxxxxxxx',  # database endpoint is written here
+        'HOST': 'database-1.cbm0ea4asfk0.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'xxxxxxxxxxxxxxxxxxxx' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'awsfinalprojectmustafa' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
