65dbcb4f8fb75ce6a52466b2ad348fe3a5235a7d
wmkorkmaz <124524338+wmkorkmaz@users.noreply.github.com>
2024-08-24 14:41:05 +0200

    Update settings.py

--- BlogDjango/src/cblog/settings.py
+++ BlogDjango/src/cblog/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'xxxxxxxx', # database name in RDS is written here
-        'USER': 'xxxxxxx', # database master username in RDS is written here
+        'NAME': 'database1', # database name in RDS is written here
+        'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
         'HOST': 'xxxxxxxxxx',  # database endpoint is written here
         'PORT': '3306' # database port is written here
